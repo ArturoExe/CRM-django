6fa562a76c0e3414b64e0727d19aca0790ca1cdd
Arturo Martinez <amarturin15@gmail>
2022-06-29 17:22:50 -0700

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home,name='home'),
+    path('products/', views.products,name='products'),
+    path('customer/<str:pk_test>', views.customer,name='customer'),
+    path('place_order/<str:pk>/', views.placeOrder,name='place_order'),
+    path('update_order/<str:pk>/', views.updateOrder,name='update_order'),
+    path('delete_order/<str:pk>/', views.deleteOrder,name='delete_order'),
+
+]
---
+++ accounts/views.py
@@ -0,0 +1,81 @@
+from .forms import OrderForm
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from .models import *
+from django.shortcuts import redirect
+from .filters import OrderFilter
+def home(request):
+    orders=Order.objects.all()
+    customers=Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+
+    delivered=orders.filter(status='Delivered').count()
+    pending=orders.filter(status="Pending").count()
+
+
+    context = {
+
+        'orders':orders,
+        'customers':customers,
+        'total_orders':total_orders,
+        'total_customers':total_customers,
+        'delivered':delivered,
+        'pending':pending
+        }
+
+    return render(request,'accounts/dashboard.html',context)
+
+def products(request):
+    products = Product.objects.all()
+    return render(request,'accounts/products.html',{'products':products})
+
+def customer(request,pk_test):
+    customer = Customer.objects.get(id=pk_test)
+    orders= customer.order_set.all()
+    order_count=orders.count()
+
+    myFilter = OrderFilter(request.GET,queryset=orders)
+    orders = myFilter.qs
+
+    context = {'customer':customer,'orders':orders,'order_count':order_count,'myFilter':myFilter}
+    return render(request,'accounts/customer.html',context)
+
+def placeOrder(request,pk):
+    OrderFormSet = inlineformset_factory(Customer,Order, fields=('product','status','note'),extra=3,can_delete=0)
+    customer = Customer.objects.get(id=pk)
+    formset=OrderFormSet(queryset=Order.objects.none(),instance=customer)
+    #form = OrderForm(initial={'customer':customer})
+    if request.method == 'POST':
+        #form=OrderForm(request.POST)
+        formset=OrderFormSet(request.POST,instance=customer)
+
+        if formset.is_valid():
+            formset.save()
+            return redirect('/')
+
+    context={'formset':formset}
+    return render(request,'accounts/order_form.html',context)
+
+def updateOrder(request,pk):
+    order = Order.objects.get(id=pk)
+    formset = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        formset=OrderForm(request.POST,instance=order)
+        if formset.is_valid():
+            formset.save()
+            return redirect('/')
+    context={'formset':formset}
+    return render(request,'accounts/order_form.html',context)
+
+def deleteOrder(request,pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    context = {'item':order}
+    return render(request,'accounts/delete.html',context)
+
